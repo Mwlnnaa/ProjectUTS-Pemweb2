ac03b915b8ceebaae56af737c663b22376b41eec
mwllnnaa <malmaulana157@gmail.com>
2025-05-05 19:23:13 +0700

    first commit

---
+++ TugasProjectUTS/proses_pasien.php
@@ -0,0 +1,41 @@
+<?php
+require_once 'dbkoneksi.php';
+
+$_kode = $_POST['kode'];
+$_nama = $_POST['nama'];
+$_tmp_lahir = $_POST['tmp_lahir'];
+$_tgl_lahir = $_POST['tgl_lahir'];
+$_gender = $_POST['gender'];
+$_email = $_POST['email'];
+$_alamat = $_POST['alamat'];
+$_kelurahan = $_POST['idkelurahan']; // sesuaikan dengan form
+$data = [$_kode, $_nama, $_tmp_lahir, $_tgl_lahir, $_gender, $_email, $_alamat, $_kelurahan];
+
+// Tambah/Ubah
+$_proses = $_POST['proses'];
+if ($_proses == "Simpan") {
+    $sql = "INSERT INTO pasien (kode,nama,tmp_lahir,tgl_lahir,gender,email,alamat,idkelurahan)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+    $st = $dbh->prepare($sql);
+    $st->execute($data);
+} else if ($_proses == "Ubah") {
+    $_id = $_POST['id']; 
+$data[] = $_id;
+$sql = "UPDATE pasien SET kode=?, nama=?, tmp_lahir=?, tgl_lahir=?, gender=?, email=?, alamat=?, idkelurahan=?
+            WHERE id=?";
+$st = $dbh->prepare($sql);
+$st->execute($data);
+}
+
+// Hapus
+if (isset($_GET['proses']) && $_GET['proses'] == 'Hapus') {
+$idx = $_GET['idx'];
+$sql = "DELETE FROM pasien WHERE id=?";
+$st = $dbh->prepare($sql);
+$st->execute([$idx]);
+}
+
+header('Location: data_pasien.php');
+
+
+?>
